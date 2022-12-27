b72065d93eb6ace1917c2ecfb9cb92a96d17088a
Nicolas <nicolas.landel@hotmail.com>
2022-12-27 12:00:08 +0100

    Create command to clean app

---
+++ backend/place/management/commands/clean_app.py
@@ -0,0 +1,25 @@
+from django.core.management.base import BaseCommand
+from place.models import Answer, Comment, Option, Place
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        # TODO improve to get all models of app
+        places = Place.objects.all()
+        answers = Answer.objects.all()
+        comments = Comment.objects.all()
+        options = Option.objects.all()
+        self.stdout.write(
+            self.style.SUCCESS(
+                f"Objtecs to be deleted: {places.count()} places, \
+                    {comments.count()} comments, {answers.count()} answers, \
+                    {options.count()} options"
+            )
+        )
+        places.delete()
+        answers.delete()
+        comments.delete()
+        options.delete()
