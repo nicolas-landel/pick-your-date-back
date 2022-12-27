e5b271f2a0a7633210da4950afb49d35f1f9ddac
Nicolas <nicolas.landel@hotmail.com>
2022-12-27 11:48:33 +0100

    Fix baseModel

--- backend/core/model_utils/base_model.py
+++ backend/core/model_utils/base_model.py
@@ -1,14 +1,23 @@
 import uuid
 
 from django.db import models
+from django.utils import timezone
 
 
 class BaseModel(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_add=True)
+    created_at = models.DateTimeField(db_index=True)
+    updated_at = models.DateTimeField(db_index=True)
 
     class Meta:
         abstract = True
         get_latest_by = "created_at"
         ordering = ("-created_at",)
+
+    def save(self, *args, **kwargs):
+        now = timezone.now()
+        if not self.created_at and self._state.adding:
+            self.created_at = now
+        if self.updated_at:
+            self.updated_at = now
+        return super().save(*args, **kwargs)
