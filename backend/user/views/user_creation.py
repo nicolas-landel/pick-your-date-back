2b6f812d99285da4d4b4676c18fc5725d2eac526
Nicolas <nicolas.landel@hotmail.com>
2023-01-08 22:27:32 +0100

    Modify login to return token

--- backend/user/views/user_creation.py
+++ backend/user/views/user_creation.py
@@ -1,6 +1,8 @@
 from rest_framework import generics
 from user.serializers import UserCreationSerializer
 
+# TODO modify and define a POST to get token
+
 
 class UserCreateView(generics.CreateAPIView):
     serializer_class = UserCreationSerializer
--- backend/user/views/user_login.py
+++ backend/user/views/user_login.py
@@ -1,17 +1,29 @@
 from django.contrib.auth import login
 from rest_framework import permissions, status, views
+from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
-from user.serializers import UserLoginSerializer
+from user.serializers import UserFullDataSerializer, UserLoginSerializer
 
 
 class UserLoginView(views.APIView):
     permission_classes = (permissions.AllowAny,)
 
     def post(self, request, format=None):
-        serializer = UserLoginSerializer(
-            data=self.request.data, context={"request": self.request}
-        )
-        serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data["user"]
-        login(request, user)
-        return Response(None, status=status.HTTP_202_ACCEPTED)
+        try:
+            serializer = UserLoginSerializer(
+                data=self.request.data, context={"request": self.request}
+            )
+            serializer.is_valid(raise_exception=True)
+            user = serializer.validated_data["user"]
+            login(request, user)
+            token, _ = Token.objects.get_or_create(user=user)
+            context = UserFullDataSerializer(user).data
+            context["token"] = token.key
+
+            return Response(
+                context,
+                status=status.HTTP_202_ACCEPTED,
+                content_type="application/json",
+            )
+        except Exception:
+            return Response(None, status=status.HTTP_403_FORBIDDEN)
