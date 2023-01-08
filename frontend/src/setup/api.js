6a16a29b965258931d6332525041f6a8f0923ce8
Nicolas <nicolas.landel@hotmail.com>
2023-01-08 22:28:45 +0100

    Create token service and init user model

--- frontend/src/setup/api.js
+++ frontend/src/setup/api.js
@@ -1,13 +1,12 @@
 
 import axios from "axios";
-
-
-console.log("ENNNNV", import.meta.env.VITE_API_URL)
+import { TokenService } from "./tokenService";
 
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   headers: {
-    "Content-type": "application/json"
+    "Content-type": "application/json",
+    "Authorization": `Token ${TokenService.getToken()}`
   },
 });
 
