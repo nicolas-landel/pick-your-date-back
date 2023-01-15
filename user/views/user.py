c85712f91100edef17ccec9862e61f730af0496c
Nicolas <nicolas.landel@hotmail.com>
2023-01-15 17:19:09 +0100

    Create error formatter and lint

---
+++ user/views/user.py
@@ -0,0 +1,11 @@
+from rest_framework.generics import RetrieveAPIView
+from rest_framework.permissions import IsAuthenticated
+
+from user.models import User
+from user.serializers import UserFullDataSerializer
+
+
+class GetUserView(RetrieveAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = User.object.all()
+    serializer_class = UserFullDataSerializer
